bc387bc3f0dbbcec10ec7327ab150cd50cd33b80
Yanis <graillot.yanis@gmail.com>
2025-05-14 16:03:39 -0600

    Fix security issue

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
-import { registerRootComponent } from 'expo';
+import { registerRootComponent } from "expo";
+// import firebase from "@react-native-firebase/app";
 
-import App from './App';
+// console.log("Firebase initialized:", firebase.app().name); // "default"
+
+import App from "./App";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
